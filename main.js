7f7d195bb977c14aad6905723868d5983e730c83
sbistransin <48774173+sbistransin@users.noreply.github.com>
2022-01-26 17:15:33 -0500

    add event listener for add-to-cart buttons

--- main.js
+++ main.js
@@ -1,6 +1,10 @@
 var viewCartButton = document.getElementsByClassName('view-cart')[0];
 var shoppingCart = document.getElementsByClassName('shopping-cart')[0];
+var products = document.getElementsByClassName('products')[0];
+var buyButtons = document.getElementsByClassName('add-to-cart');
 
+
+// my first attempt
 // viewCartButton.addEventListener('click', function () {
 //   if (shoppingCart.style.display === '') {
 //     shoppingCart.style.display = 'block';
@@ -9,10 +13,18 @@ var shoppingCart = document.getElementsByClassName('shopping-cart')[0];
 //   }
 // });
 
+// shopping cart listener
 viewCartButton.addEventListener('click', function () {
   if (shoppingCart.className.indexOf('show') === -1) {
     shoppingCart.className += ' show';
   } else {
     shoppingCart.className = 'shopping-cart';
+  }
+});
+
+// add to cart listener
+products.addEventListener('click', function (e) {
+  if (e.target.classList.contains("add-to-cart")) {
+    console.log("click button");
   }
 });
